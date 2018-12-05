db585feefe26c23a973bc4a0b0a3a93409ce67d5
Jeremy Benabu <j.benabu@mycoachsport.com>
2018-12-05 10:44:56 +0100

    fix image upload

--- MySocialApp/Utils/ImageUtils.swift
+++ MySocialApp/Utils/ImageUtils.swift
@@ -2,7 +2,12 @@ import Foundation
 
 internal struct ImageUtils {
     static func toData(_ image: UIImage) -> Data? {
-        if let file = image.jpegData(compressionQuality: CGFloat(100)) {
+        #if swift(>=4.2)
+        let file = image.jpegData(compressionQuality: CGFloat(100))
+        #else
+        let file = UIImageJPEGRepresentation(image, CGFloat(100))
+        #endif
+        if let file = file {
             return file
         } else {
             return nil
